8886b733a9ea6e5f3c20ba2074964006e7f8607d
alibberber <buyukberberal@mef.edu.tr>
2023-11-22 21:07:30 +0300

    chatgpt ile kontrol

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk  # Pillow kütüphanesi kullanılarak resim işleme
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class YanginKontrolUygulamasi:
+    def __init__(self, pencere):
+        self.pencere = pencere
+        self.pencere.title("Yangın Kontrol Uygulaması")
 
+        # Arayüz elemanlarını oluştur
+        self.label = tk.Label(pencere, text="Yapay Zeka Destekli Yangın Kontrol")
+        self.label.pack(pady=10)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        self.foto_label = tk.Label(pencere, text="Ateş Kontrolü için bir fotoğraf seçin.")
+        self.foto_label.pack(pady=10)
 
+        self.foto_button = tk.Button(pencere, text="Fotoğraf Seç", command=self.fotoSec)
+        self.foto_button.pack(pady=10)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        self.baslat_button = tk.Button(pencere, text="Başlat", command=self.baslatFonksiyonu)
+        self.baslat_button.pack(pady=10)
 
-print("hello Atamanabi")
-print("asdf")
+    def fotoSec(self):
+        # Kullanıcıya dosya seçme penceresi göster
+        dosya_yolu = filedialog.askopenfilename(filetypes=[("Resim Dosyaları", "*.png;*.jpg;*.jpeg;*.gif")])
 
-print("ALI TOPU AT")
+        # Seçilen resmi ekranda göster
+        if dosya_yolu:
+            self.gosterResim(dosya_yolu)
 
+    def gosterResim(self, dosya_yolu):
+        resim = Image.open(dosya_yolu)
+        resim = resim.resize((300, 300), Image.ANTIALIAS)
+        tk_resim = ImageTk.PhotoImage(resim)
 
-print("selam ben ali")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Eğer önceki bir resim varsa, güncelle
+        if hasattr(self, 'resim_etiketi'):
+            self.resim_etiketi.config(image=tk_resim)
+            self.resim_etiketi.image = tk_resim
+        else:
+            self.resim_etiketi = tk.Label(self.pencere, image=tk_resim)
+            self.resim_etiketi.pack(pady=10)
+
+    def baslatFonksiyonu(self):
+        # Burada fotoğraf üzerinde yangın kontrolü yapılacak işlemleri ekleyebilirsiniz.
+        pass
+
+if __name__ == "__main__":
+    pencere = tk.Tk()
+    uygulama = YanginKontrolUygulamasi(pencere)
+    pencere.mainloop()
