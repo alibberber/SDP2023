7ff9895c6f82fd2b3ae1737360dd1582a79ca2b7
alibberber <buyukberberal@mef.edu.tr>
2023-11-22 22:50:25 +0300

    dümenista hocam

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
-
+print("selam")
 class FireControlApplication:
     def __init__(self, window):
         self.window = window
