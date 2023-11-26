b4a2b529c9263afaeba68806155e538628c153d7
alibberber <buyukberberal@mef.edu.tr>
2023-11-27 00:11:07 +0300

    27 Kasım 2023

--- newdeneme.py
+++ newdeneme.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class PhotoApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Fire Detection APP")
+        self.root.title("Fotoğraf Denetleme Uygulaması")
 
         # Değişkenler
         self.image_path = None
@@ -33,40 +33,48 @@ class PhotoApp:
         self.style = ThemedStyle(self.root)
         self.style.set_theme("plastik" if not self.dark_mode.get() else "equilux")
 
+        # Ana çerçeve
+        self.main_frame = ttk.Frame(self.root)
+        self.main_frame.pack(expand=True, fill="both")
+
         # Tarih ve saat etiketi
-        self.date_label = ttk.Label(self.root, text=self.get_current_datetime(), font=('Helvetica', 10))
+        self.date_label = ttk.Label(self.main_frame, font=('Helvetica', 10))
         self.date_label.pack(side="top", anchor="ne", padx=10, pady=5)
+
+        # Sol alt köşedeki switcher butonu için çerçeve
+        self.switcher_frame = ttk.Frame(self.main_frame)
+        self.switcher_frame.pack(side="bottom", anchor="sw", padx=10, pady=10)
 
         # Arayüzü oluştur
         self.create_widgets()
 
-    def create_widgets(self):
-        # Fotoğraf yükleme butonu
-        load_button = ttk.Button(self.root, text="Fotoğraf Yükle", command=self.load_image)
-        load_button.pack(pady=10)
-
-        # Fotoğrafı gösterme alanı
-        self.image_label = ttk.Label(self.root)
-        self.image_label.pack(pady=10)
-
-        # Fotoğrafı denetleme butonu
-        process_button = ttk.Button(self.root, text="Fotoğrafı Denetle", command=self.process_image)
-        process_button.pack(pady=10)
-
-        # Koyu ve açık modu ayarlayan buton
-        mode_button = ttk.Button(self.root, text="Koyu Mod" if not self.dark_mode.get() else "Açık Mod", command=self.toggle_mode)
-        mode_button.pack(pady=10)
-
         # Tarih ve saat güncellemesi
         self.update_datetime()
 
+    def create_widgets(self):
+        # Fotoğraf yükleme butonu
+        load_button = ttk.Button(self.main_frame, text="Fotoğraf Yükle", command=self.load_image)
+        load_button.pack(pady=10)
+
+        # Fotoğrafı gösterme alanı
+        self.image_label = ttk.Label(self.main_frame)
+        self.image_label.pack(pady=10)
+
+        # Fotoğrafı denetleme butonu
+        process_button = ttk.Button(self.main_frame, text="Fotoğrafı Denetle", command=self.process_image)
+        process_button.pack(pady=10)
+
+        # Koyu ve açık modu ayarlayan buton
+        mode_button = ttk.Button(self.switcher_frame, text="Koyu Mod" if not self.dark_mode.get() else "Açık Mod", command=self.toggle_mode)
+        mode_button.pack()
+
     def load_image(self):
-        file_path = filedialog.askopenfilename(title="Fotoğraf Seç", filetypes=[("Image files", "*.png")])
+        file_path = filedialog.askopenfilename(title="Fotoğraf Seç", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
 
         if file_path:
             self.image_path = file_path
             image = Image.open(file_path)
-            image.thumbnail((300, 300))  # İstediğiniz boyutu ayarlayabilirsiniz
+            image.thumbnail((300, 300))
             photo = ImageTk.PhotoImage(image)
             self.image_label.config(image=photo)
             self.image_label.image = photo
@@ -83,21 +91,16 @@ class PhotoApp:
         # Koyu ve açık modu değiştir
         self.dark_mode.set(not self.dark_mode.get())
         self.style.set_theme("plastik" if not self.dark_mode.get() else "equilux")
-        mode_button = self.root.children['!button4']
+        mode_button = self.switcher_frame.children['!button']
         mode_button.config(text="Koyu Mod" if not self.dark_mode.get() else "Açık Mod")
-        # Koyu ve açık mod için ilgili işlemleri ekleyebilirsiniz
 
     def update_datetime(self):
         # Tarih ve saat etiketini güncelle
-        self.date_label.config(text=self.get_current_datetime())
-        # 1000 milisaniye (1 saniye) sonra güncelle
-        self.root.after(1000, self.update_datetime)
-
-    def get_current_datetime(self):
-        # Geçerli tarih ve saati al
         now = datetime.now()
         formatted_datetime = now.strftime("%d-%m-%Y %H:%M:%S")
-        return formatted_datetime
+        self.date_label.config(text=formatted_datetime)
+        # 1000 milisaniye (1 saniye) sonra güncelle
+        self.root.after(1000, self.update_datetime)
 
 if __name__ == "__main__":
     root = tk.Tk()
